8f12548ac2183a851118d1c657018752a5aed31d
Stella Wang <swang4@atlassian.com>
2022-02-17 18:08:46 +1100

    adding itemgroup

--- src/main/java/com/lysvianne/drinksbylys/item/Drinks.java
+++ src/main/java/com/lysvianne/drinksbylys/item/Drinks.java
@@ -1,6 +1,7 @@
 package com.lysvianne.drinksbylys.item;
 
 import com.lysvianne.drinksbylys.DrinksByLys;
+import com.lysvianne.drinksbylys.DrinksItemGroup;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -22,97 +23,97 @@ public class Drinks {
       () -> new Drinkable(new Item.Properties()
         .food(JUICE)
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> CARROT_JUICE = ITEMS.register("carrot_juice",
       () -> new Drinkable(new Item.Properties()
         .food(JUICE)
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> PUMPKIN_JUICE = ITEMS.register("pumpkin_juice",
       () -> new Drinkable(new Item.Properties()
         .food(JUICE)
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> SWEETBERRY_JUICE = ITEMS.register("sweetberry_juice",
       () -> new Drinkable(new Item.Properties()
         .food(JUICE)
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> CHORUS_JUICE = ITEMS.register("chorus_juice",
       () -> new Drinkable(new Item.Properties()
         .food(JUICE)
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> MELON_JUICE = ITEMS.register("melon_juice",
       () -> new Drinkable(new Item.Properties()
         .food(JUICE)
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> SUGARCANE_JUICE = ITEMS.register("sugarcane_juice",
       () -> new Drinkable(new Item.Properties()
         .food(JUICE)
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> BEETROOT_JUICE = ITEMS.register("beetroot_juice",
       () -> new Drinkable(new Item.Properties()
         .food(JUICE)
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> SPARKLING_APPLE_JUICE = ITEMS.register("sparkling_apple_juice",
       () -> new Drinkable(new Item.Properties()
         .food(buildSparklingJuice(Effects.HASTE, false))
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> SPARKLING_BEETROOT_JUICE = ITEMS.register("sparkling_beetroot_juice",
       () -> new Drinkable(new Item.Properties()
         .food(buildSparklingJuice(Effects.STRENGTH, false))
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> SPARKLING_CARROT_JUICE = ITEMS.register("sparkling_carrot_juice",
       () -> new Drinkable(new Item.Properties()
         .food(buildSparklingJuice(Effects.NIGHT_VISION, false))
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> SPARKLING_CHORUS_JUICE = ITEMS.register("sparkling_chorus_juice",
       () -> new Drinkable(new Item.Properties()
         .food(buildSparklingJuice(Effects.JUMP_BOOST, false))
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> SPARKLING_MELON_JUICE = ITEMS.register("sparkling_melon_juice",
       () -> new Drinkable(new Item.Properties()
         .food(buildSparklingJuice(Effects.INSTANT_HEALTH, true))
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> SPARKLING_PUMPKIN_JUICE = ITEMS.register("sparkling_pumpkin_juice",
       () -> new Drinkable(new Item.Properties()
         .food(buildSparklingJuice(Effects.RESISTANCE, false))
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> SPARKLING_SUGARCANE_JUICE = ITEMS.register("sparkling_sugarcane_juice",
       () -> new Drinkable(new Item.Properties()
         .food(buildSparklingJuice(Effects.SPEED, false))
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static final RegistryObject<Item> SPARKLING_SWEETBERRY_JUICE = ITEMS.register("sparkling_sweetberry_juice",
       () -> new Drinkable(new Item.Properties()
         .food(buildSparklingJuice(Effects.SLOW_FALLING, false))
         .maxStackSize(16)
-        .group(ItemGroup.FOOD)));
+        .group(DrinksItemGroup.DRINKS_GROUP)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
